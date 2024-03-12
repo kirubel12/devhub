29dfe53cfe076dbfbb9530335bbee039129e3490
kirubel12 <kirubelhabte1991@gmail.com>
2024-03-12 18:11:47 +0300

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,18 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
  
 export default authMiddleware({
   // Routes that can be accessed while signed out
   publicRoutes: ['/', '/login'],
+
+  afterAuth(auth, req, evt){
+    if (!auth.userId && !auth.isPublicRoute){
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+ 
+
+  }
 
   
 
